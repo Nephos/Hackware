d69ef6c39acf93620fa166ad3c3057081cda0705
Arthur Poulet <arthur.poulet@mailoo.org>
2017-08-17 14:31:48 +0100

    Add doc to the core

--- src/Hackware/core.cr
+++ src/Hackware/core.cr
@@ -1,3 +1,11 @@
+# The base objects are not extensibles with mods.
+# They are requirements, hooks, ...
 require "./core/base/*"
+
+# Core classes are extensibles with mods.
+# The instances of thoses classes are created from the JSON files.
+# Those instances are the models used by hackers etc. to instanciate their own objects.
 require "./core/*"
+
+# Instance objects are linked to their base properties (Core object) via a "model" id (the name of the core object). Those objects are owned by each `Person`
 require "./core/instances/*"
