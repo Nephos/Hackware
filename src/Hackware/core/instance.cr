59258f1a34ca27afba4b61fad0b85c53a650d6d5
Arthur Poulet <arthur.poulet@mailoo.org>
2017-08-18 02:04:17 +0100

    Instance.model_ref should return a Model

--- src/Hackware/core/instance.cr
+++ src/Hackware/core/instance.cr
@@ -2,7 +2,7 @@ require "./instance_json"
 
 module Core::Instance
   include Core::InstanceJSON
-  abstract def model_ref
+  abstract def model_ref : Model
   # abstract def initialize(jpp : JSON::PullParser, models : Array(Core::Model))
 end
 
