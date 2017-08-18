eaecc82c7da6b88c8039a71db0384d0e5e3b1326
Arthur Poulet <arthur.poulet@mailoo.org>
2017-08-18 17:06:50 +0100

    Improve hook specs

--- spec/core/base/hook_spec.cr
+++ spec/core/base/hook_spec.cr
@@ -1,8 +1,3 @@
-class Test::Core::Instance::Skill < Core::Instance::Skill
-  def initialize(@id, @model, @level, @xp, @model_ref)
-  end
-end
-
 describe Core::Hook do
   it "basic exec" do
     hook = Core::Hook.from_json(%({ "function": "fct_test", "mod": "mod_test" }))
@@ -13,7 +8,7 @@ describe Core::Hook do
       mods.load f.path
     end
     core_model = Core::Model::Skill.from_json %({ "name": "tmodel" })
-    core_instance = Test::Core::Instance::Skill.new id: 0_u32, model: "tmodel", level: 0_u32, xp: 0.0, model_ref: core_model
+    core_instance = Core::Instance::Skill.from_json %({ "id": 0, "model": "tmodel", "level": 0, "xp": 0.0 }), [core_model]
     hook.exec(mods, core_instance, "ok").should eq "result: 0 = ok"
   end
 end
