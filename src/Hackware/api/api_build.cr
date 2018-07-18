8805506d503116da06f6097877abb05e41860c18
Arthur Poulet <arthur.poulet@sceptique.eu>
2018-07-18 16:08:36 +0200

    Remove some logs

--- src/Hackware/api/api_build.cr
+++ src/Hackware/api/api_build.cr
@@ -8,16 +8,13 @@ module ApiBuild
   # @param args is a tuple { name: Type }
   macro create_function(env, name, *args, &block)
     {{env}}.push_global_proc({{name}}, {{args.size}}) do |ptr|
-      puts "..."
       context = Duktape::Sandbox.new ptr
-      puts "..."
       values = Array(DuktapeBuild::Any).new({{args.size}})  do |idx|
         DuktapeBuild::Cast.get_current_value(context, idx)
       end
       keys = {{args}}.map(&.to_s).to_a
 
       js_args = Hash.zip(keys, values)
-      puts js_args
 
       {{yield context, js_args}}
       context.call_success
